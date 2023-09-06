f8a6eff3a99e69d25f74a471d0328314bbe33094
Victor Villazon <victorzn23@gmail.com>
2023-09-05 22:10:52 -0500

    maquetando pagina de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { Container, Box, Paper, Avatar, Typography, TextField, Button } from '@mui/material'
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+
+const Login = () => {
+  return (
+    <Container maxWidth="xs">
+      <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10, px: 3, py: 5 }}>
+        <Avatar>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>
+          Login
+        </Typography>
+        <Box component="form">
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+          />
+          <Button type="submit" fullWidth variant="contained">
+            Sign In
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  )
+}
+
+export default Login
