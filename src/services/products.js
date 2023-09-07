f50d0af58a92fd3eba233175a741f4aa41f2e450
Victor Villazon <victorzn23@gmail.com>
2023-09-06 20:36:07 -0500

    agregando función fetchProducts para obtener los productos del endpoint

---
+++ src/services/products.js
@@ -0,0 +1,19 @@
+const ENDPOINT_URL = 'https://dummyjson.com/auth/products' // GET
+
+export const fetchProducts = async () => {
+  const user = JSON.parse(localStorage.getItem('auth'))
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authentication': `Bearer ${user.token}`
+    }
+  }
+
+  const response = await fetch(ENDPOINT_URL, options)
+
+  const data = await response.json()
+
+  return data
+}
