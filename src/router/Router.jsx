fd2647e1379062d612d97a8d5b66d548cf0f9645
Victor Villazon <victorzn23@gmail.com>
2023-09-06 19:55:22 -0500

    agregando ruta home

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import LoginPage from '../pages/Login.jsx'
+import HomePage from '../pages/Home.jsx'
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path='/login' element={<h1>LOGIN PAGE</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+
+        <Route path='/' element={<HomePage />} />
 
       </Routes>
     </BrowserRouter>
