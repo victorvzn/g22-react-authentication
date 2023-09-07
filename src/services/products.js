b467a8bea9e7b276bc34cfc4b5d7f61f6d3738ac
Victor Villazon <victorzn23@gmail.com>
2023-09-06 21:36:02 -0500

    actualizando fetchProducts para que soporte la paginación

--- src/services/products.js
+++ src/services/products.js
@@ -1,7 +1,11 @@
-const ENDPOINT_URL = 'https://dummyjson.com/auth/products' // GET
+const BASE_URL = 'https://dummyjson.com' // GET
 
-export const fetchProducts = async () => {
+// https://dummyjson.com/products?limit=5&skip=0
+
+export const fetchProducts = async (limit, skip) => {
   const user = JSON.parse(localStorage.getItem('auth'))
+
+  const RESOURCE_URL = `${BASE_URL}/auth/products?limit=${limit}&skip=${skip}`
 
   const options = {
     method: 'GET',
@@ -11,7 +15,7 @@ export const fetchProducts = async () => {
     }
   }
 
-  const response = await fetch(ENDPOINT_URL, options)
+  const response = await fetch(RESOURCE_URL, options)
 
   const data = await response.json()
 
