bb7d89ad48cbceaf52c8ef4fb7bbd2bdcbd53b82
Victor Villazon <victorzn23@gmail.com>
2023-09-06 20:08:51 -0500

    maquetando lista de productos en la pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,49 @@
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container maxWidth="md">
+      <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10, px: 3, py: 5 }}>
+        
+        <Typography variant='h5' gutterBottom>
+          Product List
+        </Typography>
+
+        <TableContainer component={Paper}>
+          <Table>
+
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Stock</TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Image</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>iPhone 9</TableCell>
+                <TableCell>S/ 549.00</TableCell>
+                <TableCell>94</TableCell>
+                <TableCell>Smarthphones</TableCell>
+                <TableCell>
+                  <img src={"https://i.dummyjson.com/data/products/1/thumbnail.jpg"} width={120} />
+                </TableCell>
+                <TableCell>
+                  Actions
+                </TableCell>
+              </TableRow>
+            </TableBody>
+
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Container>
   )
 }
 
